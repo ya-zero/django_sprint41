3d82f0f4ee927dcd18337cee9dafd793d1a6ffcd
Sergey Tsepilov <ts.sergey1994@gmail.com>
2024-04-28 23:30:28 +0500

    test_comment

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -5,7 +5,7 @@ from typing import Tuple, Any, Type, List, Union
 
 import django.test.client
 import pytest
-import pytz
+import pytz  # type: ignore
 from django.db.models import TextField, DateTimeField, ForeignKey, Model
 from django.forms import BaseForm
 from django.utils import timezone
@@ -87,7 +87,7 @@ def create_comment_creation_forms(
     except FormValidationException as e:
         raise AssertionError(
             "Убедитесь, что для валидации"
-            f" {creation_tester.of_which_form} достаточно заполнить следующие"
+            f" {creation_tester.of_which_form} достаточно заполнить следующие"  # type: ignore
             f" поля: {list(forms_to_create[0].data.keys())}. При валидации"
             f" формы возникли следующие ошибки: {e}"
         )
@@ -196,7 +196,7 @@ def test_comment(
         user_client=user_client,
         another_user_client=another_user_client,
         unlogged_client=unlogged_client,
-        **update_props,
+        **update_props,  # type: ignore
     )
 
     item_to_delete_adapter = item_to_edit_adapter
@@ -208,7 +208,7 @@ def test_comment(
         item_adapter=item_to_delete_adapter,
     ).test_delete_item(
         qs=item_to_delete_adapter.item_cls.objects.all(),
-        delete_url_addr=delete_url_addr,
+        delete_url_addr=delete_url_addr,  # type: ignore
     )
 
     status_404_on_edit_deleted_comment_err_msg = (
