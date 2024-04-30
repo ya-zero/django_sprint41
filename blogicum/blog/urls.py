216d34e80ec3590301f611e774c025a5f42c1fab
Sergey Tsepilov <ts.sergey1994@gmail.com>
2024-04-30 11:40:32 +0500

    urls

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -6,27 +6,36 @@ from . import views
 
 app_name = 'blog'
 
+# Posts
+posts = [
+    path('create/', views.create_post, name='create_post'),
+    path('<int:post_id>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/edit/', views.edit_post, name='edit_post'),
+    path('<int:post_id>/delete/', views.delete_post, name='delete_post'),
+    path('<int:post_id>/comment/', views.add_comment, name='add_comment'),
+    path(
+        '<int:post_id>/edit_comment/<int:comment_id>/',
+        views.edit_comment,
+        name='edit_comment',
+    ),
+    path(
+        '<int:post_id>/delete_comment/<int:comment_id>/',
+        views.delete_comment,
+        name='delete_comment',
+    ),
+]
+
+# Profile
+profile = [
+    path('edit/', views.edit_profile, name='edit_profile'),
+    path('<str:username>/', views.profile, name='profile'),
+]
+
 urlpatterns = [
     # Index
     path('', views.index, name='index'),
-
     # Category
-    path('category/<slug:category_slug>/',
-         views.category_posts, name='category_posts'),
-
-    # Posts
-    path('posts/create/', views.create_post, name='create_post'),
-    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('posts/<int:post_id>/edit/', views.edit_post, name='edit_post'),
-    path('posts/<int:post_id>/delete/', views.delete_post, name='delete_post'),
-    path('posts/<int:post_id>/comment/',
-         views.add_comment, name='add_comment'),
-    path('posts/<int:post_id>/edit_comment/<int:comment_id>/',
-         views.edit_comment, name='edit_comment'),
-    path('posts/<int:post_id>/delete_comment/<int:comment_id>/',
-         views.delete_comment, name='delete_comment'),
-
-    # Profile
-    path('profile/edit/', views.edit_profile, name='edit_profile'),
-    path('profile/<str:username>/', views.profile, name='profile'),
+    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
+    path('posts/', include(posts)),
+    path('profile/', include(profile)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
