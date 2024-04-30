597df1c8de28d7009f60da7df9ed9bf3c59d0de2
Sergey Tsepilov <ts.sergey1994@gmail.com>
2024-04-30 11:51:24 +0500

    urls

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -35,7 +35,8 @@ urlpatterns = [
     # Index
     path('', views.index, name='index'),
     # Category
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
+    path('category/<slug:category_slug>/',
+         views.category_posts, name='category_posts'),
     path('posts/', include(posts)),
     path('profile/', include(profile)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
