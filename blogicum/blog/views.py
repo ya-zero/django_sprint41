d04ace148e56cfd574b19ce85d88f568e93b2e44
Sergey Tsepilov <ts.sergey1994@gmail.com>
2024-04-30 11:40:45 +0500

    views

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -78,7 +78,7 @@ def profile(request, username):
     template = 'blog/profile.html'
     user = get_object_or_404(User, username=username)
     posts_list = (
-        Post.objects.filter(author=user)
+        user.posts
         .annotate(comment_count=Count('comments'))
         .order_by('-pub_date')
     )
